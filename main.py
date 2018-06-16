b0f20cdb9b18682410dcfeecd5eb774289102f8f
musaprg <conie112@gmail.com>
2018-06-17 02:35:20 +0900

    update

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import toml
 import watcher
 import signal
 import watcher
+import requests
+from argparse import ArgumentParser
 
-# w = None
+parser = ArgumentParser(description="QueueWatchClient")
+parser.add_argument("path_to_google_home", type=str, help="URL to Google Home")
+
+TARGET = None
+TRYTIMES = 3
 
 def callback(data):
     """
@@ -11,11 +17,11 @@ def callback(data):
     :param data: parsed json data (dict)
     :return:
     """
-    print(data)
-
-# def handler():
-#     global w
-#     w.stop()
+    for i in range(TRYTIMES):
+        res = requests.post(TARGET, data=data["song"]["music_url"])
+        if res.status_code == requests.codes.ok:
+            return
+    raise TimeoutError
 
 if __name__ == '__main__':
     conf = toml.load("config.toml")
@@ -26,8 +32,8 @@ if __name__ == '__main__':
     port = conf["redis-local"]["port"]
     password = None
 
-    # signal.signal(signal.SIGTERM, handler)
-    # signal.signal(signal.SIGKILL, handler)
+    args = parser.parse_args()
+    TARGET = args.path_to_google_home
 
     w = watcher.PlayQueueWatcher(host, port, password=password, debug=True)
     w.start(callback, with_thread=False)
